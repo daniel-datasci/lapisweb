ebe4142f28688f95570c754f505010ae8c4e54f1
Daniel Ifediba <danifedibah@gmail.com>
2026-08-04 14:49:13 +0100

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { lazy, Suspense } from 'react';
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
